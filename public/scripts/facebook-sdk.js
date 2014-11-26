2408380148c0de5e983dfe9fa88e1c07ea273240
Димо Петров <webdude@webdude.eu>
2014-11-27 00:54:06 +0200

    Fb like button tweaks

--- public/scripts/facebook-sdk.js
+++ public/scripts/facebook-sdk.js
@@ -1,7 +1,8 @@
-(function(d, s, id) {
+(function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
+    js = d.createElement(s);
+    js.id = id;
     js.src = "//connect.facebook.net/bg_BG/sdk.js#xfbml=1&appId=134955213354516&version=v2.0";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
