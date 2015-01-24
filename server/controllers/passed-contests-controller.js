a43dbe22b1e7f14f996e796254edc104d4c37e7c
Dimo Petrov <dimo.petrov@sap.com>
2015-01-24 15:58:26 +0200

    Added a few methods to the Data

--- server/controllers/passed-contests-controller.js
+++ server/controllers/passed-contests-controller.js
@@ -1,18 +1,21 @@
 var cloudinary = require('cloudinary');
 var q = require('q');
 var data = require('../data');
-var CLOUDINARY_UPLOAD_FOLDER_NAME = 'contestants';
 var CONTROLLER_NAME = 'passed-contests';
-var PAGE_SIZE = 10;
-var INVALID_IMAGE_ERROR = 'Моля уверете се, че сте избрали валидно ' +
-    'изображение от следните формати (gif, jpg, jpeg, tiff, png)!';
 cloudinary.config(process.env.CLOUDINARY_URL);
 
 module.exports = {
-    getPassedContests: function (req, res, next) {
+    getAll: function (req, res, next) {
         var deferred = q.defer();
-        res.render(CONTROLLER_NAME + '/all');
-        deferred.resolve();
-        return deferred.promise;
+
+        data.passedContests.getAll(
+            function (err) {
+                deferred.reject();
+                res.redirect('/not-found');
+            },
+            function (passedContests) {
+                res.render(CONTROLLER_NAME + '/all', passedContests);
+                deferred.resolve();
+            });
     }
 };
--- server/routes/passed-contests.js
+++ server/routes/passed-contests.js
@@ -2,7 +2,7 @@ var controllers = require('../controllers');
 
 module.exports = function (app) {
     app.route('/passed-contests/all')
-        .get(controllers.passedContests.getPassedContests);
+        .get(controllers.passedContests.getAll);
 
     app.route('/contestants/:id')
         .get();
