f8d8d4d9ca56a4a235bce5844470025c0ee5129d
webdude21 <webdude@webdude.eu>
2015-03-02 13:59:52 +0200

    Fixed refactoring that went wrong (thank god for tests). Added client side script compilation to gitignore

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -68,7 +68,7 @@ module.exports = {
                     }, function (user) {
                         req.logIn(user, function (err) {
                             if (err) {
-                                req.session.errorMessage = "A terrible error has occurred! " + err.text();
+                                req.session.errorMessage = "A terrible error has occurred! " + err.err.toString();
                                 res.redirect('/error');
                             }
                             res.redirect('/');
