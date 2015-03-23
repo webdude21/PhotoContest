79e2ec673fedfc04e92b31f8add8568cfefa0997
webdude21 <webdude@webdude.eu>
2015-03-23 19:33:36 +0200

    Fixed an error in the client side script

--- public/src/scripts/responsive-images.js
+++ public/src/scripts/responsive-images.js
@@ -1,10 +1,12 @@
 (function () {
-    var $galleryContainer = $('#participants');
-    $galleryContainer.masonry({
-        itemSelector: '.item'
-    });
+    $(document).ready(function () {
+        var $galleryContainer = $('#participants');
+        $galleryContainer.masonry({
+            itemSelector: '.item'
+        });
 
-    $galleryContainer.find('img').load(function () {
-        $galleryContainer.masonry();
+        $galleryContainer.find('img').load(function () {
+            $galleryContainer.masonry();
+        });
     });
 }());
