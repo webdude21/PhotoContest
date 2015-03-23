581fa998beb5f9d59b06e4e62057856d86e3338a
webdude21 <webdude@webdude.eu>
2015-03-23 19:36:52 +0200

    Restored accidantly deleted files

---
+++ public/src/scripts/responsive-images.js
@@ -0,0 +1,12 @@
+(function () {
+    $(document).ready(function () {
+        var $galleryContainer = $('#participants');
+        $galleryContainer.masonry({
+            itemSelector: '.item'
+        });
+
+        $galleryContainer.find('img').load(function () {
+            $galleryContainer.masonry();
+        });
+    });
+}());
