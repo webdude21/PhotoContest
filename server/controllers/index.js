e2d6f0df8b6cb613805ea9da2c7ecc363cd16883
webdude <webdude@webdude.eu>
2015-07-14 23:16:19 +0300

    litle polishing

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,8 @@
 'use strict';
 
 var exports = {};
-require('../utilities/helpers').autoRequireFiles(__dirname,
-    function (keyName, transformedFileName) {
-        exports[keyName] = require('./' + transformedFileName);
-    });
+require('../utilities/helpers').autoRequireFiles(__dirname, function (keyName, transformedFileName) {
+    exports[keyName] = require('./' + transformedFileName);
+});
 
 module.exports = exports;
