83af7c9a5ef65d356cfdca074287b2d8e6593c90
webdude <webdude@webdude.eu>
2015-07-21 23:49:01 +0300

    fixed tests configuration

--- tests/controllers/contestants.js
+++ tests/controllers/contestants.js
@@ -1,38 +1,43 @@
 require('../../server/config/mongoose')(require('../../server/config/config')['development']);
-var data = require('../../server/data'),
-    sinon = require('sinon'),
+var sinon = require('sinon'),
+    chai = require('chai'),
+    sinonChai = require('sinon-chai'),
     controllers = require('../../server/controllers');
+chai.should();
+chai.use(sinonChai);
 
 function getExpressMock() {
-    var req, res, spy;
+    var req, res;
 
     req = res = {};
     req.query = {};
-    spy = res.render = sinon.spy();
+    res.render = sinon.spy();
 
     return {
-        req: req, res: res, spy: spy
+        request: req, response: res
     };
 }
 
 describe("#Contestants Controller", function () {
     describe("Get Approved Contestants", function () {
-        it("should render some the result", function () {
+        it("should render some the result", function (testDoneCallBack) {
             var express = getExpressMock();
-            controllers.contestants.getAllApproved(express.req, express.res)
+            controllers.contestants.getAllApproved(express.request, express.response)
                 .then(function () {
-                    express.spy.render.should.toHaveBeenCalled();
-                });
+                    express.response.render.should.have.been.calledWith('contestants/all');
+                    testDoneCallBack();
+                }).done(null, testDoneCallBack);
         });
     });
 
     describe("Get Register Contestant", function () {
-        it("should return register form", function () {
+        it("should return register form", function (testDoneCallBack) {
             var express = getExpressMock();
-            controllers.contestants.getRegister(express.req, express.res)
+            controllers.contestants.getRegister(express.request, express.response)
                 .then(function () {
-                    express.spy.render.should.toHaveBeenCalled();
-                });
+                    express.response.render.should.have.been.calledWith('contestants/register');
+                    testDoneCallBack();
+                }).done(null, testDoneCallBack);
         });
     });
 });
