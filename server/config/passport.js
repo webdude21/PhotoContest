e08df383f2eb430674d48deeae217cecfbf07168
webdude21 <dimo.petrof@gmail.com>
2015-11-28 15:42:05 +0200

    moved some stuff around

--- server/config/passport.js
+++ server/config/passport.js
@@ -3,32 +3,33 @@ var passport = require('passport'),
     FacebookStrategy = require('passport-facebook').Strategy,
     User = require('mongoose').model('User'),
     data = require('../data'),
-    encryption = require('../utilities/encryption');
+    encryption = require('../utilities/encryption'),
+    registerFacebookUser = function (accessToken, refreshToken, profile, done) {
+        var fbUser = {
+            facebookId: profile.id,
+            firstName: profile._json.first_name,
+            lastName: profile._json.last_name,
+            username: profile.username ? profile.username : profile._json.first_name + '_' + profile._json.last_name
+        };
+
+        fbUser.salt = encryption.generateSalt();
+        fbUser.hashPass = encryption.generateHashedText(fbUser.salt, encryption.generateSalt());
+
+        // very dumb solution here, but FB users don't always come with emails
+        fbUser.email = profile.emails ? profile.emails[0].value : `${fbUser.facebookId}@facebook.com`;
+
+        data.userService.findOrCreate(fbUser, (user) => {
+            return done(null, user);
+        });
+    },
+    fbStrategy = new FacebookStrategy({
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_APP_SECRET,
+        callbackURL: process.env.BASE_URL + '/auth/facebook/callback'
+    });
 
 module.exports = function () {
-    passport.use(new FacebookStrategy({
-            clientID: process.env.FACEBOOK_APP_ID,
-            clientSecret: process.env.FACEBOOK_APP_SECRET,
-            callbackURL: process.env.BASE_URL + '/auth/facebook/callback'
-        }, function (accessToken, refreshToken, profile, done) {
-            var fbUser = {
-                facebookId: profile.id,
-                firstName: profile._json.first_name,
-                lastName: profile._json.last_name,
-                username: profile.username ? profile.username : profile._json.first_name + '_' + profile._json.last_name
-            };
-
-            fbUser.salt = encryption.generateSalt();
-            fbUser.hashPass = encryption.generateHashedText(fbUser.salt, encryption.generateSalt());
-
-            // very dumb solution here, but FB users don't always come with emails
-            fbUser.email = profile.emails ? profile.emails[0].value : `${fbUser.facebookId}@facebook.com`;
-
-            data.userService.findOrCreate(fbUser, (user) => {
-                return done(null, user);
-            });
-        }
-    ));
+    passport.use(fbStrategy, registerFacebookUser);
     passport.use(new LocalPassport(function (username, password, done) {
         User.findOne({username}).exec(function (err, user) {
             if (err) {
