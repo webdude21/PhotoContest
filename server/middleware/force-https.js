755ca7f1c39527319551ecfe1b9ac90ac72771db
webdude21 <dimo.petrof@gmail.com>
2016-01-09 12:38:04 +0200

    add some documentation for the force-http middleware

--- server/middleware/force-https.js
+++ server/middleware/force-https.js
@@ -1,3 +1,7 @@
+/**
+ * middleware to force https for heroku reverse proxy enviorment.
+ * @function
+ */
 module.exports = function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
         return res.redirect(['https://', req.get('Host'), req.url].join(''));
