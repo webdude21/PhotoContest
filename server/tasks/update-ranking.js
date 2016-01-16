5baa9f86b4c47cf28513c8dd59becc922222db58
webdude21 <dimo.petrof@gmail.com>
2016-01-16 17:24:46 +0200

    removed q from yet another place

--- server/tasks/update-ranking.js
+++ server/tasks/update-ranking.js
@@ -1,19 +1,15 @@
 var fb = require('fb'),
-    q = require('q'),
     ACCESS_TOKEN = `${process.env.FACEBOOK_APP_ID}|${process.env.FACEBOOK_APP_SECRET}`,
     baseUrl = process.env.BASE_URL,
     getUserVotes = function (participantId) {
-        var deferred = q.defer();
-
-        fb.api('/', 'get', {id: `${baseUrl}/contestants/${participantId}`}, function (res) {
-            if (res.error) {
-                return deferred.reject(res.error);
-            }
-
-            deferred.resolve(res.shares || 0);
+        return new Promise(function (resolve, reject) {
+            fb.api('/', 'get', {id: `${baseUrl}/contestants/${participantId}`}, function (res) {
+                if (res.error) {
+                    return reject(res.error);
+                }
+                resolve(res.shares || 0);
+            });
         });
-
-        return deferred.promise;
     },
     saveUserVotes = function (contestant) {
         getUserVotes(contestant._id)
